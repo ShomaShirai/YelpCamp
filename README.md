c30b9f39f9297009036219b8ec70297376f8e773
{shirai} <{22t0837a@student.gs.chiba-u.jp}>
2025-02-01 11:39:09 +0900

    エラーハンドリングを行った．主に，新規登録や変更

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# YelpCamp(Udemy)
+# YelpCamp(Udemy)：キャンプ場サイト
 
 ## 開発環境
 - Windows 11 WSL2
@@ -28,7 +28,15 @@ $ sudo systemctl stop mongod
 - useCreateIndexは，mongoose6.0以降で使わない
 - ミドルウェアとはリクエストとレスポンスの間で，ターミナルにログを出力，パスワードのチェックで使える
 - bootstrap5はjQueryの依存関係が無くなった
+- expressのエラー処理でミドルウェアを用いる
+- expressにはデフォルトでエラー処理をするミドルウェアがある
+- サーバーサイドでエラーハンドリングを行わないようにする
+- エラーハンドリングは画面を使わずリクエストをポストする場合(サーバーサイドからのリクエスト)の対策を行うべきである
+- Joiはスキーマのバリデーションを行う
+- リレーションとはデータベースの関係
+- 一対多ではテーブルが二つ必要であるが，多対多の場合はテーブルが三つ必要である
 
 # 参考資料(公式ドキュメント第一)
 - MongoDB(https://www.mongodb.com/ja-jp/docs/manual/installation/)
 - Mongoose(javascript)(https://mongoosejs.com/)
+- Expressの公式ドキュメント(https://expressjs.com/ja/guide/writing-middleware.html)
--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+const { campgroundSchema } = require('./schemas');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
-const ejsMate= require('ejs-mate')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp',
-    { useNewUrlParser: true, useUnifiedTopology: true})
+    { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('MongoDBコネクションOK！！');
     })
@@ -17,52 +20,75 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp',
 
 const app = express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.engine('ejs', ejsMate);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(detail => detail.message).join(',');
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
 
 app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.get('/campgrounds', async (req, res) => {
+app.get('/campgrounds', catchAsync(async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds });
-});
+}));
 
 app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 });
 
-app.get('/campgrounds/:id', async (req, res) => {
+app.get('/campgrounds/:id', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', { campground });
-});
+}));
 
-app.post('/campgrounds', async (req, res) => {
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res) => {
+    // if (!req.body.campground) throw new ExpressError('不正なキャンプ場のデータです', 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
-});
+}));
 
-app.get('/campgrounds/:id/edit', async (req, res) => {
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/edit', { campground });
-});
+}));
 
-app.put('/campgrounds/:id', async (req, res) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`);
-});
+}));
 
-app.delete('/campgrounds/:id', async (req, res) => {
+app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+}));
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('ページが見つかりませんでした', 404));
+});
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) {
+        err.message = '問題が起きました'
+    }
+    res.status(statusCode).render('error', { err });
 });
 
 app.listen(3000, () => {
